98c6ba4681a5bef52e2dfee3cf2fc5975f96f3e1
Teaca Sebastian <sebiteaca@gmail.com>
2023-01-06 09:43:17 +0200

    Add boilerplate style and form components

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="d-flex align-items-center vh-100">
+      <div className='container'>
+        <div className="w-95 shadow-sm mx-auto">
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit voluptatum doloribus cum consequatur minus, quia distinctio. Mollitia vel consectetur facilis?</p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
